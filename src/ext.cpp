c304a7698eefab549772e86f7ef74ce2f962568f
Markus Worchel <9449513+mworchel@users.noreply.github.com>
2026-01-14 13:43:48 +0100

    Add first draft of forward-mode derivatives

--- src/ext.cpp
+++ src/ext.cpp
@@ -208,6 +208,102 @@ void voxelize_2d_cpu(Float const* vertices, uint32_t num_vertices,
     delete[] weight;
 }
 
+template<typename Float, unsigned int N>
+Point<Float, N> point_to_grid_coord(Point<Float, N> const& x, Vector<Float, N> const& voxel_size)
+{
+    Point<Float, N> const grid_origin(-1);
+    return (x - grid_origin) / voxel_size + Point<Float,N>(0);
+}
+
+// Forward-mode derivatives of the smooth indicator function
+template<typename Float>
+void voxelize_2d_forward_cpu(Float const* vertices, uint32_t num_vertices,
+                             uint32_t const* edges, uint32_t num_edges,
+                             Float* occupancy, uint32_t height, uint32_t width,
+                             Float const* d_vertices,
+                             Float* d_occupancy,
+                             uint32_t num_samples_per_simplex, 
+                             Filter<Float> filter)
+{
+    std::default_random_engine            engine(std::random_device{}());
+    std::uniform_real_distribution<Float> distribution;
+
+    // TODO: Lift assumption of grid being in [-1,1]^3
+    Vector2<Float> const voxel_size{
+        Float(2) / width,
+        Float(2) / height
+    };
+
+    // Compute the boundary term (integration over edges)
+    for (uint32_t e = 0; e < num_edges; ++e)
+    {
+        uint32_t const v0_idx = edges[2*e + 0];
+        uint32_t const v1_idx = edges[2*e + 1];
+
+        Point2<Float> const v0{
+            vertices[2*v0_idx + 0],
+            vertices[2*v0_idx + 1],
+        };
+
+        Point2<Float> const v1{
+            vertices[2*v1_idx + 0],
+            vertices[2*v1_idx + 1],
+        };
+
+        Vector2<Float> const v0v1 = v1 - v0;
+
+        // 90 degree rotation to the right
+        Vector2<Float> const normal = normalize(Vector2<Float>{v0v1[1], -v0v1[0]});
+
+        Float const area = norm(v0v1);
+
+        // Monte Carlo integration over the edge
+        // TODO: Stratified sampling?
+        // TODO: Quadrature?
+        for (uint32_t sample_index = 0; sample_index < num_samples_per_simplex; ++sample_index)
+        {
+            Float const u = distribution(engine);
+            // x_b = (1-u) * v0 + u * v1 = v0 + u * (v1 - v0)
+            Vector2<Float> const d_v0{d_vertices[2 * v0_idx + 0], d_vertices[2 * v0_idx + 1]};
+            Vector2<Float> const d_v1{d_vertices[2 * v1_idx + 0], d_vertices[2 * v1_idx + 1]};
+
+            Float const xb0 = dot(d_v0, normal * (1 - u));
+            Float const xb1 = dot(d_v1,  normal * u);
+
+            Point2<Float> const sample            = v0 + u * v0v1;
+            Point2<Float> const sample_grid_coord = point_to_grid_coord(sample, voxel_size);
+
+            // TODO: Make function
+            Float const filter_support[2] = {
+                filter.radius / voxel_size[0],
+                filter.radius / voxel_size[1]};
+
+            int32_t const min_x = floor(sample_grid_coord[0] - filter_support[0]);
+            int32_t const max_x = floor(sample_grid_coord[0] + filter_support[0]);
+            int32_t const min_y = floor(sample_grid_coord[1] - filter_support[1]);
+            int32_t const max_y = floor(sample_grid_coord[1] + filter_support[1]);
+
+            for (int32_t ny = std::max(min_y, 0); ny < std::min(static_cast<int32_t>(height), max_y + 1); ++ny)
+            {
+                for (int32_t nx = std::max(min_x, 0); nx < std::min(static_cast<int32_t>(width), max_x + 1); ++nx)
+                {
+                    // Compute the weight using the distance from the sample to the voxel center
+                    Point2<Float> const n_center{
+                        (nx + Float(0.5)) * voxel_size[0] - Float(1),
+                        (ny + Float(0.5)) * voxel_size[1] - Float(1)};
+
+                    Float const filter_weight = filter.eval(n_center - sample);
+                    uint64_t const n_voxel_index = width * ny + nx;
+                    if (filter_weight != Float(0))
+                    {
+                        d_occupancy[n_voxel_index] += filter_weight * (xb0 + xb1) * area / Float(num_samples_per_simplex); // TODO: the normalization is incorrect
+                    }
+                }
+            }
+        }
+    }
+}
+
 template<typename Float>
 void voxelize_3d_cpu(Float const* vertices, uint32_t num_vertices,
                      uint32_t const* faces, uint32_t num_faces,
@@ -352,6 +448,41 @@ void voxelize(nb::ndarray<Float, nb::c_contig> const &vertices,
                                     occupancy.data(), occupancy.shape(0), occupancy.shape(1), occupancy.shape(2), num_samples_per_voxel);
 }
 
+template <typename Float>
+void voxelize_forward(nb::ndarray<Float, nb::c_contig> const &vertices,
+                      nb::ndarray<uint32_t, nb::c_contig> const &simplices,
+                      nb::ndarray<Float, nb::c_contig> &occupancy,
+                      nb::ndarray<Float, nb::c_contig> const &d_vertices,
+                      nb::ndarray<Float, nb::c_contig> &d_occupancy,
+                      uint32_t num_samples_per_simplex, Float filter_radius)
+{
+    if (vertices.ndim() != 2 || (vertices.shape(1) != 2 && vertices.shape(1) != 3))
+        throw std::invalid_argument(format_message("Expected vertices with shape (n,2) or (n,3) but array has %d dimensions", vertices.ndim()));
+
+    if (simplices.ndim() != 2 || (simplices.shape(1) != 2 && simplices.shape(1) != 3))
+        throw std::invalid_argument(format_message("Expected simplices with shape (n,2) or (n,3) but array has %d dimensions", simplices.ndim()));
+
+    if (vertices.shape(1) != simplices.shape(1))
+        throw std::invalid_argument(format_message("Expected vertices (dim=%d) and simplices (dim=%d) with same dimension", vertices.shape(1), simplices.shape(1)));
+
+    if (occupancy.ndim() != 2 && occupancy.ndim() != 3)
+        throw std::invalid_argument(format_message("Expected occupancy grid with shape (h,w) or (d,h,w) but array has %d dimensions", occupancy.ndim()));
+
+    check_on_device(vertices.device_type(), vertices.device_id(), vertices, simplices, occupancy);
+
+    unsigned int N = vertices.shape(1);
+
+    dvx::Filter<Float> filter{
+        .type   = dvx::FilterType::Box,
+        .radius = filter_radius};
+
+    if (vertices.shape(1) == 2)
+        dvx::voxelize_2d_forward_cpu<Float>(vertices.data(), vertices.shape(0), simplices.data(), simplices.shape(0),
+                                            occupancy.data(), occupancy.shape(0), occupancy.shape(1), 
+                                            d_vertices.data(), d_occupancy.data(),
+                                            num_samples_per_simplex, filter);
+}
+
 NB_MODULE(dvx_ext, m)
 {
 #if NDEBUG
@@ -365,4 +496,5 @@ NB_MODULE(dvx_ext, m)
     m.def("unmute", [=]() { suppress_warnings = false; });
 
     m.def("voxelize_f32", voxelize<float>, nb::arg("vertices"), nb::arg("simplices"), nb::arg("occupancy"), nb::arg("num_samples_per_voxel"), nb::arg("filter_radius"));
+    m.def("voxelize_forward_f32", voxelize_forward<float>, nb::arg("vertices"), nb::arg("simplices"), nb::arg("occupancy"), nb::arg("d_vertices"), nb::arg("d_occupancy"), nb::arg("num_samples_per_simplex"), nb::arg("filter_radius"));
 }
