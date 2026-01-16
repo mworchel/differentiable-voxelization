b414a7966b0ec9d4927844d04028f203c0a214fd
Markus Worchel <9449513+mworchel@users.noreply.github.com>
2026-01-16 17:23:58 +0100

    Expose extension build type through `dvx` package

---
+++ dvx/extension.py
@@ -0,0 +1,6 @@
+import diffmoments_ext as dmx
+
+__all__ = ["extension_build_type"]
+
+def extension_build_type():
+    return dmx.build_type()
--- dvx/torch/__init__.py
+++ dvx/torch/__init__.py
@@ -1,1 +1,4 @@
 from .voxelize import *
+
+# Expose some helpful functions from the (parent) extension module
+from dvx.extension import extension_build_type
